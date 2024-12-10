589a23c8261469e4aa53a43b5142633476d24bf8
UtsavH <Utsavhudka2002@gmail.com>
2024-12-10 14:36:41 -0400

    updated app.js static folder

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());  // This should be placed before routes
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // Use routes
 app.use('/', indexRouter);
